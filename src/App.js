c9006e81936aedd772c403e634f4bf1794a50554
KishanSinghChauhan <kishansingh705446@gmail.com>
2020-12-20 01:08:27 +0530

    add spotify api setup with login | [Kishan]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import Login from "./components/Login/Login";
+import { getTokenFromResponse } from './spotify'
 function App() {
+  const [token, setToken] = useState(null)
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = '';
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token);
+    }
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+           <h1>Logged In</h1>
+          ) : (
+            <Login />
+          )
+        }
     </div>
   );
 }
