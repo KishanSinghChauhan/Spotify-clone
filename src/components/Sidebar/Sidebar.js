3f7661f6695c91904cacaaa941d83e45f8553f4c
KishanSinghChauhan <kishansingh705446@gmail.com>
2020-12-20 14:06:06 +0530

    add sidebar with options and styles | [Kishan]

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import './Sidebar.scss';
-const Sidebar = () => {
-    return (
-        <div>
-            <h1>Sidebar</h1>
-        </div>
-    )
+import React from "react";
+import "./Sidebar.scss";
+import SidebarOption from "./SidebarOption/SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SidebarOption Icon={HomeIcon} option="Home" />
+      <SidebarOption Icon={SearchIcon} option="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
+      <br />
+    </div>
+  );
 }
 
 export default Sidebar;
