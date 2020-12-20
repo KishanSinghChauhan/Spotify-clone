d2f0e39c3b385858f9367f925d0bf16e30e53603
KishanSinghChauhan <kishansingh705446@gmail.com>
2020-12-20 15:41:42 +0530

    add songRow and header data | [Kishan]

---
+++ src/components/MainInfo/SongRow/SongRow.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./SongRow.scss";
+
+const SongRow = () => {
+  return (
+    <div className="songRow">
+      <img className="songRow__album" src= alt="" />
+      <div className="songRow__info">
+        <h1>name</h1>
+        <p>
+          song name
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SongRow;
